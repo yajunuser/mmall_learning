73fd64b525320b2d0770213f9f8d888e9b38083e
亚军 <zyjtoto@163.com>
2019-06-10 22:02:20 +0800

    保存

---
+++ src/main/java/com/mymmall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.mymmall.dao;
+
+import com.mymmall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
